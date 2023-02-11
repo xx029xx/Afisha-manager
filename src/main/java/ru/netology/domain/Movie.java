6f4e573ef40fc7f356b5fc0f3de3273167c4cbb2
Natalia Zvereva <xx029xx@ya.ru>
2023-02-11 11:39:05 +0300

    Manager movie

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Movie {
+
+    private int id;
+    private String name;
+    private String genre;
+    private boolean premiereTomorrow;
+
+}
+
